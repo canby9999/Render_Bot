f2d3b0dbf84a73fb16ecc6ad818a48332c10b64d
canby9999 <121427032+canby9999@users.noreply.github.com>
2023-10-17 10:16:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import ConfigParser
+import configparser
 import random
 
 app = Flask(__name__)
 
-config = ConfigParser.ConfigParser()
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
 config.read('config.ini')
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 
+# 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -36,9 +38,9 @@ def callback():
 def prettyEcho(event):
 
     sendString = ""
-    if "AAA" in event.message.text:
+    if "擲筊" in event.message.text:
         sendString = divinationBlocks()
-    elif "BBB" in event.message.text or "B" in event.message.text:
+    elif "抽簽" in event.message.text or "抽" in event.message.text:
         sendString = drawStraws()
     else:
         sendString = event.message.text 
@@ -49,11 +51,11 @@ def prettyEcho(event):
     )
 
 def divinationBlocks():
-    divinationBlocksList = ["A01", "A02", "A03", "A04"] 
+    divinationBlocksList = ["笑杯", "正杯", "正杯", "笑杯"] 
     return divinationBlocksList[random.randint(0, len(divinationBlocksList) - 1)]
 
 def drawStraws():
-    drawStrawsList = ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08"]
+    drawStrawsList = ["大吉", "中吉", "小吉", "吉", "凶", "小凶", "中凶", "大凶"]
     return drawStrawsList[random.randint(0, len(drawStrawsList) - 1)]
 
 if __name__ == "__main__":
