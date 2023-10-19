c24d68e2ea673f5c904936ac4caff5b74aeb9c96
canby9999 <121427032+canby9999@users.noreply.github.com>
2023-10-19 10:55:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,67 @@
-from __future__ import unicode_literals
-import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import configparser
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import openai
+import time
 import random
+import yfinance as yf
+#======python的函數庫==========
 
 app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+# OPENAI API Key初始化設定
+# openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# LINE 聊天機器人的基本資料
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+def GPT_response(text):
+    # 接收回應
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    print(response)
+    # 重組回應
+    answer = response['choices'][0]['text'].replace('。','')
+    return answer
 
 
-# 接收 LINE 的資訊
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+    # handle webhook body
     try:
-        print(body, signature)
+        #\\ print(body, signature)
         handler.handle(body, signature)
-
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
+      
 
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
+
 def prettyEcho(event):
 
     sendString = ""
-    if "擲筊" in event.message.text:
-        sendString = divinationBlocks()
-    elif "抽簽" in event.message.text or "抽" in event.message.text:
+    if "列表" in event.message.text:
+        sendString = listYFsymbol()
+    elif "BBB" in event.message.text or "B" in event.message.text:
         sendString = drawStraws()
     else:
         sendString = event.message.text 
@@ -50,13 +71,20 @@ def prettyEcho(event):
         TextSendMessage(text=sendString)
     )
 
-def divinationBlocks():
-    divinationBlocksList = ["笑杯", "正杯", "正杯", "笑杯"] 
-    return divinationBlocksList[random.randint(0, len(divinationBlocksList) - 1)]
+def listYFsymbol():
+    yfList = ["^TWII", "2330.TW", "TWD=X"] 
+    history = "<List>\r"
+    for symbol in yfList:
+        stock = yf.Ticker(symbol)        
+        data = stock.history(period="1d")
+        history += symbol + ":" + data + "\r"
+    return history
 
 def drawStraws():
-    drawStrawsList = ["大吉", "中吉", "小吉", "吉", "凶", "小凶", "中凶", "大凶"]
+    drawStrawsList = ["BB1", "BB2", "BB3", "BB4", "BB5", "BB6", "BB7", "BB8"]
     return drawStrawsList[random.randint(0, len(drawStrawsList) - 1)]
-
+                              
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
